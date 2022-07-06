4fd80ae752b673dbc8c213f4c42d7745e184a3d4
abelAsres <abelasres35@gmail.com>
2022-07-06 15:17:55 -0400

    added custom exception

--- src/test/java/com/cognizant/feign/controllers/FemsControllerTest.java
+++ src/test/java/com/cognizant/feign/controllers/FemsControllerTest.java
@@ -1,7 +1,10 @@
 package com.cognizant.feign.controllers;
 
+import com.cognizant.feign.exceptions.CustomFeignException;
 import com.cognizant.feign.services.ProductService;
 import com.cognizant.feign.services.ProductServiceImpl;
+import feign.FeignException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,9 +12,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FemsControllerTest {
@@ -30,5 +36,28 @@ class FemsControllerTest {
   void getProductByName() {
    femsController.findProductByName("shirt");
    Mockito.verify(productService, times(1)).findProductByName("shirt");
+  }
+
+  @Test
+  void getProductByName_FeignException_404() {
+    CustomFeignException e = new CustomFeignException(404,"You have an error");
+    when(productService.findProductByName("shirts")).thenThrow(e);
+    ResponseEntity<?> actual = femsController.findProductByName("shirts");
+    assertThat(actual.getStatusCodeValue()).isEqualTo(404);
+  }
+
+  @Test
+  void getProductByName_FeignException_400() {
+    CustomFeignException e = new CustomFeignException(400,"You have an error");
+    when(productService.findProductByName("shirts")).thenThrow(e);
+    ResponseEntity<?> actual = femsController.findProductByName("shirts");
+    assertThat(actual.getStatusCodeValue()).isEqualTo(400);
+  }
+
+  @Test
+  void getProductByName_IllegalArgumentException() {
+    when(productService.findProductByName("shirts")).thenThrow(IllegalArgumentException.class);
+    ResponseEntity<?> actual = femsController.findProductByName("shirts");
+    assertThat(actual.getStatusCodeValue()).isEqualTo(400);
   }
 }
